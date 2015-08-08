7071db7168904df04c30f8f33208cbfffd2aef44
ahwin <ahmadul.hassan@gmail.com>
2015-08-08 10:19:34 -0400

    FAB scroll behavior added

--- app/src/main/java/com/megaphone/skoozi/MainActivity.java
+++ app/src/main/java/com/megaphone/skoozi/MainActivity.java
@@ -281,6 +281,7 @@ public class MainActivity extends AppCompatActivity
             nearbyMap.addMarker(new MarkerOptions()
                     .position(questionLocation));
         }
+
     }
 
     private void updateNearbyList(List<Question> questions) {
