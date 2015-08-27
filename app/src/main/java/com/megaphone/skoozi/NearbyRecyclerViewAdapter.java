2d263a3302cce11bff88a72c274ffb664c041104
ahwin <ahmadul.hassan@gmail.com>
2015-08-27 01:17:43 -0400

    user images show up as letters

--- app/src/main/java/com/megaphone/skoozi/NearbyRecyclerViewAdapter.java
+++ app/src/main/java/com/megaphone/skoozi/NearbyRecyclerViewAdapter.java
@@ -1,13 +1,17 @@
 package com.megaphone.skoozi;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -39,6 +43,7 @@ public class NearbyRecyclerViewAdapter extends RecyclerView.Adapter<NearbyRecycl
         TextView nearbyTimestamp;
         TextView nearbyContent;
         TextView nearbyUserName;
+        ImageView nearbyNameImage;
 
         private OnQuestionItemSelected mQuestionItemCallback;
         /**
@@ -50,6 +55,7 @@ public class NearbyRecyclerViewAdapter extends RecyclerView.Adapter<NearbyRecycl
             nearbyTimestamp = (TextView) itemView.findViewById(R.id.nearby_list_question_duration);
             nearbyUserName = (TextView) itemView.findViewById(R.id.nearby_list_profile_name);
             nearbyContent = (TextView) itemView.findViewById(R.id.nearby_list_question);
+            nearbyNameImage = (ImageView) itemView.findViewById(R.id.nearby_list_name_image);
 //            this.context = context;
             mQuestionItemCallback = (OnQuestionItemSelected) context;
             itemView.setOnClickListener(this);
@@ -102,6 +108,15 @@ public class NearbyRecyclerViewAdapter extends RecyclerView.Adapter<NearbyRecycl
         holder.nearbyUserName.setText(questionItem.author);
         holder.nearbyContent.setText(questionItem.content);
         holder.displayOnMap(questionItem.locationLat, questionItem.locationLon);
+
+        //todo: decision to display user image or letter should be made here
+        ColorGenerator generator = ColorGenerator.MATERIAL; // or use DEFAULT
+        // generate random color
+        int nameImageColor = generator.getRandomColor();
+        TextDrawable nameDrawable = TextDrawable.builder()
+                .buildRound(questionItem.author.substring(0,1).toUpperCase(), nameImageColor);
+        holder.nearbyNameImage.setImageDrawable(nameDrawable);
+        holder.nearbyNameImage.setVisibility(View.VISIBLE);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
