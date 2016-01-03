c7f639125a8fe13c1a10e04cbcc3834405736a3b
ahwin <ahmadul.hassan@gmail.com>
2016-01-03 00:53:49 -0500

    multidex not required

--- app/src/main/java/com/megaphone/skoozi/SkooziApplication.java
+++ app/src/main/java/com/megaphone/skoozi/SkooziApplication.java
@@ -3,7 +3,6 @@ package com.megaphone.skoozi;
 import android.accounts.Account;
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
 
 import com.megaphone.skoozi.util.AccountUtil;
 
@@ -18,12 +17,6 @@ public class SkooziApplication extends Application {
     // Returns the application instance
     public static SkooziApplication getInstance() {
         return singleton;
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
     }
 
     @Override
