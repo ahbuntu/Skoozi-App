54e7c5ab64e43cbff2b5b696834287678e452751
ahwin <ahmadul.hassan@gmail.com>
2016-01-03 00:13:42 -0500

    Fixed app gradle multi dex build issue

--- app/src/main/java/com/megaphone/skoozi/SkooziApplication.java
+++ app/src/main/java/com/megaphone/skoozi/SkooziApplication.java
@@ -3,6 +3,7 @@ package com.megaphone.skoozi;
 import android.accounts.Account;
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.megaphone.skoozi.util.AccountUtil;
 
@@ -17,6 +18,12 @@ public class SkooziApplication extends Application {
     // Returns the application instance
     public static SkooziApplication getInstance() {
         return singleton;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
     @Override
