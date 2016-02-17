f2243335d8a26c787aec30ed25aa925113dbe88d
ahwin <ahmadul.hassan@gmail.com>
2016-02-16 22:38:21 -0500

    beginning to bring in flipboard bottomsheet

--- app/src/main/java/com/megaphone/skoozi/UserAccountActivity.java
+++ app/src/main/java/com/megaphone/skoozi/UserAccountActivity.java
@@ -130,14 +130,10 @@ public class UserAccountActivity extends BaseActivity implements OnMapReadyCallb
             nicknameEditContainer.setVisibility(View.VISIBLE);
             nicknameEdit.addTextChangedListener(new TextWatcher() {
                 @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
                 @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                }
+                public void onTextChanged(CharSequence s, int start, int before, int count) { }
 
                 @Override
                 public void afterTextChanged(Editable s) {
